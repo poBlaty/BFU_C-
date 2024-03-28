1c62866d00510f751133e9852b52e7c21f0961ea
tr3zv1y <alexey.veliky@gmail.com>
2024-03-28 23:55:46 +0200

    mend

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <string>
 #include "math-figures/Square.h"
 using namespace mt;
 
 int main() {
-    Parallelogram *p = new Parallelogram(4,5,30);
-    std::cout << (*p).getPerimeter() << std::endl;
-    Rectangle *r = new Rectangle(4, 5);
-    std::cout << (*r).getLengthDiagonal() << std::endl;
-    Square *s = new Square(5);
-    std::cout << (*s).getLengthDiagonal() << std::endl;
+    try {
+        Parallelogram *p = new Parallelogram(4, 5, 30);
+        std::cout << (*p).getPerimeter() << std::endl;
+        Rectangle *r = new Rectangle(4, 5);
+        std::cout << (*r).getLengthDiagonal() << std::endl;
+        Square *s = new Square(-5);
+        std::cout << (*s).getLengthDiagonal() << std::endl;
+    } catch(std::string str) {
+        std::cout << str;
+    }
 
 }
--- math-figures/Parallelogram.cpp
+++ math-figures/Parallelogram.cpp
@@ -4,9 +4,12 @@
 #define PI 3.14159
 using namespace mt;
 
-Parallelogram::Parallelogram(float side1, float side2, double _angle) {
+Parallelogram::Parallelogram(float side1, float side2, double _angle){
     if (_angle > 180 || _angle < 0) {
         throw std::invalid_argument{"the angle is not within from 0 to 180"};
+    }
+    if (side1 <= 0 || side2 <= 0) {
+        throw std::invalid_argument{"Any sides is less or equal 0"};
     }
     this->side_1 = side1;
     this->side_2 = side2;
