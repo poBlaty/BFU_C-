79c724f802fb9a41f6b61f49901a4bd8d1e1716c
tr3zv1y <alexey.veliky@gmail.com>
2024-03-29 00:55:35 +0200

    first com

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using std::cout;
+
+class Matrix{
+    unsigned int N;
+    unsigned int M;
+
+    Matrix(unsigned int n, unsigned int m){
+        this->N = n;
+        this->M = m;
+
+    }
+    // умножение на число
+    // произведение матриц
+    // разность матриц
+
+    // сумма матриц
+
+    // определитель
+    // конструктор копирования
+
+    //оператор присваивания копированием
+
+    // деструктор
+};
+
+int main() {
+
+    return 0;
+}
